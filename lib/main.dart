11a4702f6b3c25d8369f2c0e8c3a5636672e5c9a
Hemshree100 <31439735+Hemshree100@users.noreply.github.com>
2019-07-05 21:12:19 +0530

    Add files via upload

--- lib/main.dart
+++ lib/main.dart
@@ -7,9 +7,9 @@ class App extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-        title: 'Flutter Demo',
+        title: 'Tic Tac Toe',
         theme: ThemeData(
-          primarySwatch: Colors.teal,
+          primarySwatch: Colors.blue,
           brightness: Brightness.dark,
 
         ),
